979087c565c9bba44aced4d87f47fd77becfb083
Sunny Lan <sunny.lan@uwaterloo.ca>
2021-07-24 15:38:33 -0400

    Make saveablemat threadsafe

--- GenshinBotCore/data/SavableMat.cs
+++ GenshinBotCore/data/SavableMat.cs
@@ -12,22 +12,28 @@ namespace genshinbot.data
     public class SavableMat : SavableMatSubset
     {
         private Mat? _value;
+        private object lck = new object();
         public Mat Value
         {
             get
             {
-                //TODO double read may occur
-                if (_value is not null) return _value;
-                else
+                lock (lck)
                 {
-                    _value = Data.Imread(this.Path, this.ImreadMode ?? ImreadModes.Color);
-                    return _value;
+                    if (_value is not null) return _value;
+                    else
+                    {
+                        _value = Data.Imread(this.Path, this.ImreadMode ?? ImreadModes.Color);
+                        return _value;
+                    }
                 }
             }
             set
             {
-                _value?.Dispose();
-                _value = value;
+                lock (lck)
+                {
+                    _value?.Dispose();
+                    _value = value;
+                }
             }
         }
         ~SavableMat()
